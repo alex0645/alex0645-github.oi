60088afb40a7bd58fcefb6714428dd5c881bfab9
alex0645 <39711974+alex0645@users.noreply.github.com>
2018-09-27 12:13:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+
+ 
+
 function g()
 
 {
 
 if(document.getElementById("sl1").selectedIndex==0)
 document.getElementById("pic1").src="2.jpg";
- 
 
 
 
 if(document.getElementById("sl1").selectedIndex==1)
 document.getElementById("pic1").src="3.jpeg";
- width=20%
 
 
 
