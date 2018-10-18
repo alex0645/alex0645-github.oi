df4237a45095c3bda9cbad7db5926d5280d8e078
alex0645 <39711974+alex0645@users.noreply.github.com>
2018-10-18 11:36:16 +0300

    Update j32.js

--- j32.js
+++ j32.js
@@ -1,43 +1,38 @@
-﻿(function () {
-    'use strict';
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
 
-    // отдаем referrer и другие данные статистике
-    chrome.runtime.onMessage.addListener(function (req, sender, sendResponse) {
-        req = req || {};
-        if (req.contentScriptAction === 'getBarnavigParams') {
-            var data = {
-                referer: document.referrer || null,
-                yamm: req.isYandexHost ? getYamm() : null
-            };
-
-            // try..catch чтобы избежать script error line 0, возможный фикс VBCHROME-329
-            try {
-                sendResponse(data);
-            } catch (e) {}
-
-            /*
-            getSpeedMetrics(function (speedData) {
-                if (speedData) {
-                    data.t = speedData;
-                }
-
-                // try..catch чтобы избежать script error line 0, возможный фикс VBCHROME-329
-                try {
-                    sendResponse(data);
-                } catch(e) {}
-            });
-            return true;
-            */
-        }
-    });
-
-    function getYamm() {
-        var metaNodes = document.getElementsByTagName('meta');
-        for (var i = 0, len = metaNodes.length; i < len; i++) {
-            var metaName = (metaNodes[i].name || '').toLowerCase();
-            if (metaName == 'yamm' && metaNodes[i].content) {
-                return String(metaNodes[i].content).substr(0, 10);
-            }
-        }
-    }
-})();
+function f2()
+{
+        a=parseFloat($('tx1').value);
+	b=parseFloat($('tx2').value);
+        x1=a;
+        x2=b;
+	if(a>b)
+{$('tx3').value=''+a;
+}
+else
+{$('tx3').value=''+b;
+}
+}
+function f1()
+{
+        a=parseFloat($('tx1').value);
+	b=parseFloat($('tx2').value);
+        x1=a;
+        x2=b;
+	if(a<b)
+{$('tx3').value=''+a;
+}
+else
+{$('tx3').value=''+b;
+}
+}
